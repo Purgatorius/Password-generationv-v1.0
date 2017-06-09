21734af5de31eef89409f38e0b893da8f8588a60
Purgatorius <Bartlomiej.Bugala@gmail.com>
2017-06-09 12:25:00 +0200

    Create code_PasswordGenerator.java

---
+++ code_PasswordGenerator.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by B4RT on 2017-06-09.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("It is a program to generate a password from a-z. Give me lenght of password you would like to have: ");
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int r;
+        for (int i = 0; i < a; i++)
+        {
+            r = random.nextInt(26) + 97;
+            stringBuilder.append((char)r);
+        }
+        System.out.println("Your password is: ");
+        System.out.println(stringBuilder.toString());
+    }
+}
